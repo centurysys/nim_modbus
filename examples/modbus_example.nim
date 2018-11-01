0c0b47845077a3dc1cbfda8c982b9345907e650c
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2018-11-01 14:41:24 +0900

    add some functions.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- examples/modbus_example.nim
+++ examples/modbus_example.nim
@@ -1,4 +1,4 @@
-import nim_modbus as modbus
+import ../src/nim_modbus as modbus
 
 when isMainModule:
   let ctx = modbus.new_rtu("/dev/ttyS0", B9600, PARITY_NONE, 8, 1)
@@ -7,6 +7,13 @@ when isMainModule:
     echo "libmodbus_new_rtu OK!"
     var ser_type = ctx.rtu_get_serial_mode()
     echo ser_type
+
+    var timeout = ctx.get_byte_timeout()
+    echo timeout
+    timeout *= 2
+    ctx.set_byte_timeout(timeout)
+    timeout = ctx.get_byte_timeout()
+    echo timeout
 
     discard ctx.set_slave(1)
     discard ctx.connect()
--- src/nim_modbus.nim
+++ src/nim_modbus.nim
@@ -1,3 +1,6 @@
+import posix
+import sequtils
+
 when defined(unix):
   const libname = "libmodbus.so.5"
 
@@ -10,6 +13,11 @@ when defined(unix):
 type
   ModbusStruct {.final, pure.} = object
   Modbus* = ptr ModbusStruct
+
+  Timeval* {.importc: "struct timeval", header: "<sys/select.h>",
+            final, pure.} = object
+    tv_sec*: clong
+    tv_usec*: clong
 
 type
   RtuMode* = enum
@@ -24,7 +32,7 @@ type
     B19200 = 19200
     B38400 = 38400
     
-
+# RTU Context
 proc modbus_new_rtu(device: cstring, baud: cint, parity: char,
                     data_bit: cint, stop_bit: cint): Modbus {.libmodbus, samename.}
 
@@ -33,38 +41,59 @@ proc new_rtu*(device: cstring, baud: SerialBaud, parity: SerialParity,
   result = modbus_new_rtu(device, cast[cint](baud), cast[char](parity),
                           cast[cint](data_bit), cast[cint](stop_bit))
 
-proc free*(ctx: Modbus) {.libmodbus, prefixed.}
-
 proc modbus_rtu_get_serial_mode(ctx: Modbus): cint {.libmodbus, samename.}
 
 proc modbus_rtu_set_serial_mode(ctx: Modbus, mode: cint): cint {.libmodbus, samename.}
 
 proc rtu_get_serial_mode*(ctx: Modbus): RtuMode =
-  var t: int = ctx.modbus_rtu_get_serial_mode()
-  if t == 0:
+  var mode: int = ctx.modbus_rtu_get_serial_mode()
+  if mode == 0:
     result = MODBUS_RTU_RS232C
   else:
     result = MODBUS_RTU_RS485
 
 proc rtu_set_serial_mode*(ctx: Modbus, mode: RtuMode): int =
   var mode: cint = case mode:
-    of MODBUS_RTU_RS232C:
-      0
-    of MODBUS_RTU_RS485:
-      1
-    else:
-      raise newException(ValueError, "Invalid Mode")
+  of MODBUS_RTU_RS232C:
+    0
+  of MODBUS_RTU_RS485:
+    1
+  else:
+    raise newException(ValueError, "Invalid Mode")
 
   result = ctx.modbus_rtu_set_serial_mode(mode)
 
+# TCP(IPv4) Context
+proc new_tcp*(ip: cstring, port: cint): Modbus {.libmodbus, prefixed.}
+
+# Common
+proc free*(ctx: Modbus) {.libmodbus, prefixed.}
+
 proc set_slave*(ctx: Modbus, slave: cint): cint {.libmodbus, prefixed.}
 
+# Connection
 proc connect*(ctx: Modbus): cint {.libmodbus, prefixed.}
 
 proc close*(ctx: Modbus) {.libmodbus, prefixed.}
 
 proc flush*(ctx: Modbus) {.libmodbus, prefixed.}
 
+proc modbus_get_byte_timeout(ctx: Modbus, byte_timeout: ptr Timeval) {.libmodbus, samename.}
+
+proc get_byte_timeout*(ctx: Modbus): int =
+  var tv: Timeval
+  modbus_get_byte_timeout(ctx, addr(tv))
+  result = (int(tv.tv_sec) * 1_000 + int(tv.tv_usec) div 1_000)
+
+proc modbus_set_byte_timeout(ctx: Modbus, timeout: ptr Timeval) {.libmodbus, samename.}
+
+proc set_byte_timeout*(ctx: Modbus, timeout: int) =
+  var tv: Timeval
+  tv.tv_sec = cast[clong](timeout div 1_000)
+  tv.tv_usec = cast[clong]((timeout %% 1_000) * 1_000)
+  modbus_set_byte_timeout(ctx, addr(tv))
+
+# Client
 proc modbus_read_input_registers(ctx: Modbus, address: cint, nb: cint,
                                  dest: pointer): cint {.libmodbus, samename.}
 
